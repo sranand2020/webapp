b567ebc62677e7f65935c6df1b2b62fc17938df9
Anand Raj S <sranand2020@gmail.com>
2024-11-09 10:04:01 +0530

    Completed the read-expenses

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,16 @@
+import ExpenseList from "../../components/ExpenseList";
+import useExpenses from "../../hooks/useExpenses";
+
+const Dashboard = () => {
+  const { expenses, error, isLoading } = useExpenses();
+
+  return (
+    <div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p style={{ color: "red" }}>Error: {error}</p>}
+      <ExpenseList expenses={expenses} />
+    </div>
+  );
+};
+
+export default Dashboard;

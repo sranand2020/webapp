98ee0740bf720be31344409902486929f2cb42ef
Anand Raj S <sranand2020@gmail.com>
2024-11-11 15:13:58 +0530

    Completed the Dashboard Status

---
+++ src/pages/dashboard/DashboardStatus.tsx
@@ -0,0 +1,31 @@
+import CurrencyUtils from "../../utils/CurrencyUtils";
+import DateUtils from "../../utils/DateUtils";
+
+interface Props {
+  loggedInUser: string;
+  totalExpenses: number;
+}
+
+const DashboardStatus = ({ loggedInUser, totalExpenses }: Props) => {
+  return (
+    <div className="mt-2">
+      <div className="text-center">
+        <h5 className="mb-4">Total Expenses</h5>
+        <h3>
+          <span className="badge rounded-pill app-primary-background-color">
+            {CurrencyUtils.formatToINR(totalExpenses)}
+          </span>
+        </h3>
+      </div>
+      <div className="d-flex justify-content-between">
+        <div>
+          Welcome, <b className="app-primary-color">{loggedInUser}</b>
+        </div>
+        <div>{DateUtils.getFormattedDate(new Date())}</div>{" "}
+        {/* Corrected rendering of the current date */}
+      </div>
+    </div>
+  );
+};
+
+export default DashboardStatus;

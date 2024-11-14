e3adc98da844c340250cb9bcf1a8a12a8bacc765
Anand Raj S <sranand2020@gmail.com>
2024-11-14 10:39:55 +0530

    Completed read by expense id

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Expense } from "../model/Expense";
 import CurrencyUtils from "../utils/CurrencyUtils";
 import DateUtils from "../utils/DateUtils";
@@ -35,7 +36,11 @@ const ExpenseList = ({ expenses }: Props) => {
 
       <div className="card-body">
         {expenses.map((expense) => (
-          <div key={expense.expenseId}>
+          <Link
+            key={expense.expenseId}
+            to={`/view/${expense.expenseId}`}
+            style={{ textDecoration: "none" }}
+          >
             <div className="d-flex justify-content-between border-bottom-1 p=3 text-dark">
               <div className="card-title m-0">
                 <h5>{expense.name}</h5>
@@ -49,7 +54,7 @@ const ExpenseList = ({ expenses }: Props) => {
                 </span>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>

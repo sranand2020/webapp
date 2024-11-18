adc1e984db03b96dd54b39fdbbf929612f23f289
Anand Raj S <sranand2020@gmail.com>
2024-11-18 10:29:40 +0530

    completed delete functionality

--- src/pages/expense/ExpenseDetails.tsx
+++ src/pages/expense/ExpenseDetails.tsx
@@ -1,7 +1,10 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import CurrencyUtils from "../../utils/CurrencyUtils";
 import DateUtils from "../../utils/DateUtils";
 import useExpenseByExpenseId from "../../hooks/useExpenseByExpenseId";
+import ConfirmDialog from "../../components/ConfirmDialog";
+import { useState } from "react";
+import { deleteExpenseByExpenseId } from "../../services/expense-service";
 
 const ExpenseDetails = () => {
   const { expenseId } = useParams<{ expenseId: string }>();
@@ -10,14 +13,48 @@ const ExpenseDetails = () => {
     <p className="text-danger">Invalid Expense Id</p>;
   }
 
-  const { expense, errors, isLoading } = useExpenseByExpenseId(expenseId!);
+  const { expense, errors, isLoading, setLoader, setErrors } =
+    useExpenseByExpenseId(expenseId!);
+  const [showDialog, setShowDialog] = useState<boolean>(false);
+
+  const navigate = useNavigate();
+
+  const handleCancel = () => {
+    console.log("Cancel is Clicked");
+    setShowDialog(false);
+  };
+  const handleConfirm = () => {
+    console.log("Deleting expense with ID:", expenseId);
+    setLoader(true);
+    deleteExpenseByExpenseId(expenseId)
+      .then((response) => {
+        if (response && response.status === 204) {
+          navigate("/");
+          console.log(response);
+        }
+      })
+      .catch((error) => {
+        setErrors(error.message);
+        console.log("Error:", error.response);
+      })
+      .finally(() => {
+        setLoader(false);
+        setShowDialog(false);
+      });
+    console.log("Confirm is Clicked");
+  };
 
   return (
     <div className="container mt-2">
       {isLoading && <p>Loading...</p>}
       {errors && <p className="text-danger">{errors} </p>}
       <div className="d-flex flex-row-reverse mb-2">
-        <button className="btn btn-sm btn-danger">DELETE</button>
+        <button
+          className="btn btn-sm btn-danger"
+          onClick={() => setShowDialog(true)}
+        >
+          DELETE
+        </button>
         <button className="btn btn-sm btn-warning mx-2">EDIT</button>
         <Link className="btn btn-sm btn-secondary" to="/">
           BACK
@@ -55,6 +92,13 @@ const ExpenseDetails = () => {
           </table>
         </div>
       </div>
+      <ConfirmDialog
+        title="Confirm Delete"
+        message="Are you sure you want to delete this item?"
+        show={showDialog}
+        onCancel={handleCancel}
+        onConfirm={handleConfirm}
+      />
     </div>
   );
 };
--- src/services/expense-service.ts
+++ src/services/expense-service.ts
@@ -5,6 +5,10 @@ export const getExpenses = () => {
   return apiClient.get<Expense[]>("/expenses");
 };
 
-export const getExpenseByExpenseId = (expenseId) => {
+export const getExpenseByExpenseId = (expenseId: string) => {
   return apiClient.get<Expense>(`/expenses/${expenseId}`);
+};
+
+export const deleteExpenseByExpenseId = (expenseId: string) => {
+  return apiClient.delete<void>(`expenses/${expenseId}`);
 };
